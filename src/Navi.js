1f4b26d7a7907c6467c7bebe1eace5213f4f073c
Ridvan Yuksek <ridvanxyuksek@gmail.com>
2024-04-06 23:40:03 +0200

    ReactKurs

---
+++ src/Navi.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import CartSummery from './CartSummery';
+
+export default class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Northwind App</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} style={{ flexGrow: '0' }} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem >
+                <NavLink>
+                <Link to="/form1">Form Demo 1</Link>
+                </NavLink>
+                
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <CartSummery cart={this.props.cart} removeFromCart={this.props.removeFromCart} />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
